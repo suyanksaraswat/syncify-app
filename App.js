9e6c1101f50942798d0df3d66995b617d7f333e5
Stenio Wagner <stenio.wagner1@gmail.com>
2020-04-03 19:30:53 -0300

    chore: update project to use expo

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import EntryFile from './src'
 
 export default function App() {
   return (
-    <View style={styles.container}><Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}>
+      <EntryFile />
     </View>
   );
 }
