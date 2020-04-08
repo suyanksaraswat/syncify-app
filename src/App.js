99285ef152da26c3f1f2c82047dcfeade2b1b54d
Stenio Wagner <stenio.wagner1@gmail.com>
2020-04-08 20:07:41 -0300

    chore: setup custom font

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { ThemeProvider } from 'styled-components';
-
+import { Text } from 'react-native';
 import Navigation from './routes/routes';
 import client from './config/client';
 import theme from './styles';
@@ -13,7 +13,14 @@ const App = () => (
     <ApolloProvider
       client={client}
     >
-      <Navigation />
+      <Text
+        style={{
+          fontFamily: 'CircularStd-Black',
+          fontSize: 18,
+        }}
+      >
+        Hello!
+      </Text>
     </ApolloProvider>
   </ThemeProvider>
 );
