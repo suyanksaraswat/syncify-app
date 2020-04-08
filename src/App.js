114cddcc109d21eca292dac46040d136b35ac9f4
Stenio Wagner <stenio.wagner1@gmail.com>
2020-04-08 12:13:45 -0300

    chore: setup apollo-graphql

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
 import { ThemeProvider } from 'styled-components';
 
 import Navigation from './routes/routes';
+import client from './config/client';
 import theme from './styles';
 
 const App = () => (
   <ThemeProvider
     theme={theme}
   >
-    <Navigation />
+    <ApolloProvider
+      client={client}
+    >
+      <Navigation />
+    </ApolloProvider>
   </ThemeProvider>
 );
 
