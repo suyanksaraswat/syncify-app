1d274bd424ea8e817300ca67b6285c1bd95c7965
Caio Kinzel Filho <caiokf@gmail.com>
2020-05-06 00:16:01 -0300

    Podcasts reducer

--- src/modules/reducers.js
+++ src/modules/reducers.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import player from './player/reducer'
+import podcasts from './podcasts/reducer'
 
 export default combineReducers({
 	player,
+	podcasts,
 })
