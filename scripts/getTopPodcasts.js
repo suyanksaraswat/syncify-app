e01bee2bef2dc6d4015771786956fba2d6ead996
Natalia Filippova <fnd.natalia@gmail.com>
2020-05-25 18:51:43 -0300

    Ádded a missing script

--- scripts/getTopPodcasts.js
+++ scripts/getTopPodcasts.js
@@ -1,34 +1,36 @@
 // Scrap itunes TOP # podcasts and save in JSON to local files
-const fetch = require('node-fetch');
-const podcastFeedParser = require("podcast-feed-parser")
-var fs = require('fs');
+const fetch = require('node-fetch')
+const podcastFeedParser = require('podcast-feed-parser')
+const fs = require('fs')
 
-var path = './src/modules/podcasts/data-new/';
-var numOfPodcasts = 100;
+const path = './src/modules/podcasts/data-new/'
+const numOfPodcasts = 100
 
-async function getAndSaveJson (url,filename) {
-    const podcast = await podcastFeedParser.getPodcastFromURL(url);
-    fs.writeFile(path+filename, JSON.stringify(podcast), function(err) {
-        if (err) {
-            console.log(err);
-        }
-    });
+async function getAndSaveJson(url, filename) {
+	const podcast = await podcastFeedParser.getPodcastFromURL(url)
+	fs.writeFile(path + filename, JSON.stringify(podcast), function (err) {
+		if (err) {
+			throw err
+		}
+	})
 }
 
-console.log(`Obtaining top ${numOfPodcasts} Podcasts from ITUNES and saving to ${path} ...`);
-
-fetch(`http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppodcasts/limit=${numOfPodcasts}/json`)
-    .then(response => response.json())
-    .then(data => {
-        data.feed.entry.forEach(element => {
-            let id = element.id.attributes['im:id']
-            fetch(`https://itunes.apple.com/lookup?entity=podcast&id=${id}`)
-                .then(response => response.json())
-                .then(data => {
-                    let podcast = data.results[0].collectionName;
-                    let feedURL = data.results[0].feedUrl;
-                    let filename = podcast.replace(/[^a-z0-9]/gi, '_').toLowerCase() + '.json';
-                    getAndSaveJson(feedURL,filename);
-                });
-        })
-    });
+fetch(
+	`http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppodcasts/limit=${numOfPodcasts}/json`
+)
+	.then((response) => response.json())
+	.then((data) => {
+		data.feed.entry.forEach((element) => {
+			const id = element.id.attributes['im:id']
+			fetch(`https://itunes.apple.com/lookup?entity=podcast&id=${id}`)
+				.then((response) => response.json())
+				.then((resData) => {
+					const podcast = resData.results[0].collectionName
+					const feedURL = resData.results[0].feedUrl
+					const filename =
+						`${podcast.replace(/[^a-z0-9]/gi, '_').toLowerCase() 
+						}.json`
+					getAndSaveJson(feedURL, filename)
+				})
+		})
+	})
