6e387ad3a43659b7de1223af8cd743d5994de873
Natalia Filippova <fnd.natalia@gmail.com>
2020-05-27 22:47:06 -0300

    button name

--- src/components/screens/Auth/SignIn/index.jsx
+++ src/components/screens/Auth/SignIn/index.jsx
@@ -1,14 +1,12 @@
 import * as React from 'react'
-import { Text, Button } from 'react-native'
+import { Button } from 'react-native'
 import Screen from '@app/components/layout/Screen'
 import { connect } from 'react-redux'
 import { login } from '@app/modules/auth/actions'
 
 const SignIn = (props) => (
 	<Screen>
-		<Text>Sign In</Text>
-
-		<Button title="Log in with Auth0" onPress={props.login} />
+		<Button title="Log in" onPress={props.login} />
 	</Screen>
 )
 export default connect(() => ({}), {
