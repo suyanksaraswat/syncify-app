6ef82dada30a880914ba4b30c47d1b5d18b745a8
Natalia Filippova <fnd.natalia@gmail.com>
2020-05-27 23:07:02 -0300

    clean-up: emoved previous mock data of podcasts

--- src/modules/constants.js
+++ src/modules/constants.js
@@ -3,8 +3,3 @@ export const PLAY_STATE = {
 	PAUSED: 'paused',
 	STOPPED: 'stopped',
 }
-
-export const PODCAST_TITLE = {
-	JOE_ROGAN: 'The Joe Rogan Experience',
-	FREAKONOMICS: 'Freakonomics Radio',
-}
