dee3d3b9a4d87c4c52fa1c7300f03f35d195e5bf
Natalia Filippova <fnd.natalia@gmail.com>
2020-06-12 00:36:41 -0300

    resolving conflicts with development branch

--- src/modules/social/actions.js
+++ src/modules/social/actions.js
@@ -1,7 +1,20 @@
 import searchResults from './data/searchResults'
+import { GetAllUsersQuery } from '@app/graphql/queries/getAllUsers'
+import { client } from '@app/graphql/client'
 
 export const CONNECTIONS_SEARCH_SUCCESS = 'CONNECTIONS_SEARCH_SUCCESS'
 
 export const search = () => async (dispatch) => {
 	dispatch({ type: CONNECTIONS_SEARCH_SUCCESS, payload: searchResults })
+
+
+export const getAllUsers = () => async (dispatch) => {
+	const response = await client.query({
+		query: GetAllUsersQuery,
+	})
+
+	dispatch({
+		type: 'GET_ALL_USERS',
+		payload: { users: response.data.users },
+	})
 }
--- src/modules/social/reducer.js
+++ src/modules/social/reducer.js
@@ -1,14 +1,30 @@
 import { CONNECTIONS_SEARCH_SUCCESS } from './actions'
 
-const initialState = {
+export const initialState = {
+	allUsers: null,
 	results: [],
 }
 
 export default function reducer(state = initialState, action) {
 	switch (action.type) {
-		case CONNECTIONS_SEARCH_SUCCESS:
-			return { results: action.payload }
-		default:
-			return state
+		case CONNECTIONS_SEARCH_SUCCESS: {
+			return {
+				...state,
+				results: action.payload
+			}
+		}
+
+		case 'GET_ALL_USERS': {
+			return {
+				...state,
+				allUsers: action.payload.users,
+			}
+		}
+
+		default: {
+			return {
+				...state,
+			}
+		}
 	}
 }
