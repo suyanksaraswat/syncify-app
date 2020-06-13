0c55e1d31486511f3acba04fdfc4c6af7078dda7
samjam48 <s.w.h@live.co.uk>
2020-06-13 16:52:19 +0100

    dynamically write list of podcasts for easy update

--- scripts/getTopPodcasts.js
+++ scripts/getTopPodcasts.js
@@ -3,16 +3,23 @@ const fetch = require('node-fetch')
 const fs = require('fs')
 const podcastFeedParser = require('podcast-feed-parser')
 
-const path = '../src/modules/podcasts/data/'
+const path = '../src/modules/podcasts'
 const numOfPodcasts = 100
 
-async function getAndSaveJson(url, filename) {
+const podcastsIndex = []
+
+async function getAndSaveJson(url, name) {
 	const podcast = await podcastFeedParser.getPodcastFromURL(url)
-	fs.writeFile(path + filename, JSON.stringify(podcast), function (err) {
-		if (err) {
-			throw err
+	fs.writeFile(
+		`${path}/data/${name}.json`,
+		JSON.stringify(podcast),
+		function (err) {
+			if (err) {
+				throw err
+			}
+			podcastsIndex.push(`require('./data/${name}.json')`)
 		}
-	})
+	)
 }
 
 fetch(
@@ -27,10 +34,25 @@ fetch(
 				.then((resData) => {
 					const podcast = resData.results[0].collectionName
 					const feedURL = resData.results[0].feedUrl
-					const filename = `${podcast
+					const name = podcast
 						.replace(/[^a-z0-9]/gi, '_')
-						.toLowerCase()}.json`
-					getAndSaveJson(feedURL, filename)
+						.toLowerCase()
+					getAndSaveJson(feedURL, name)
 				})
 		})
 	})
+
+setTimeout(
+	() =>
+		fs.writeFile(
+			`${path}/podcastList.js`,
+			`/* eslint-disable global-require */
+export default [${podcastsIndex}]`,
+			function (err) {
+				if (err) {
+					throw err
+				}
+			}
+		),
+	100000
+)
