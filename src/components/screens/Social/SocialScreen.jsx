5318954dbb3aa8ab4e131bdb2278296b9cb7aed1
Natalia Filippova <fnd.natalia@gmail.com>
2020-06-15 23:53:01 -0300

    cleaned up SocialScreen

--- src/components/screens/Social/SocialScreen.jsx
+++ src/components/screens/Social/SocialScreen.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
-import { View, Text, FlatList, ScrollView } from 'react-native'
+import { ScrollView } from 'react-native'
 import Screen from '@app/components/layout/Screen'
 import SearchBar from '@app/components/common/SearchBar'
-import Button from '@app/components/common/Button'
 import Results from './Results'
 import { HeaderBox, HeaderText } from './styles'
 import FriendsList from './FriendsList'
@@ -16,32 +15,8 @@ const SocialScreen = (props) => {
 			<ScrollScreen>
 				<HeaderBox>
 					<HeaderText>My Connections</HeaderText>
-					<Button
-						onPress={() => {
-							props.getAllUsers()
-						}}
-						name="account-plus"
-						size={35}
-					/>
 				</HeaderBox>
 				<FriendsList />
-				{props.allUsers.length > 0 && (
-					<HeaderBox>
-						<HeaderText>Add More Friends</HeaderText>
-					</HeaderBox>
-				)}
-				{props.allUsers && (
-					<FlatList
-						data={props.allUsers}
-						numColumns={1}
-						renderItem={({ item }) => (
-							<View key={item.id} onPress={() => {}}>
-								<Text>{item.username}</Text>
-							</View>
-						)}
-						keyExtractor={(item) => item.id}
-					/>
-				)}
 			</ScrollScreen>
 		)
 	}
